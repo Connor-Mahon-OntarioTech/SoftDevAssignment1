07cd5ea4276d97314635862fd0627ea5722462e1
Connor Mahon <connor.mahon@ontariotechu.net>
2020-03-03 01:17:32 -0500

    Completed Question 4

--- src/question4/Main.java
+++ src/question4/Main.java
@@ -8,12 +8,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
 
 import java.io.*;
 import java.util.Scanner;
@@ -30,11 +28,26 @@ public class Main extends Application {
         HBox hbox = new HBox(20);
         hbox.setAlignment(Pos.CENTER);
 
-        calculateOccurrence();
         graph.drawGraph();
+        hbox.getChildren().add(new Label("Filename:"));
+        TextField filename = new TextField();
+        hbox.getChildren().addAll(filename);
         Button btGraph = new Button("Graph");
         hbox.getChildren().addAll(btGraph);
-        //hbox.getChildren().add(new Label("Filename:"));
+
+        btGraph.setOnAction(e -> {
+            calculateOccurrence(filename.getText());
+            graph.drawGraph();
+        });
+
+        filename.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case ENTER:
+                    calculateOccurrence(filename.getText());
+                    graph.drawGraph();
+                    break;
+            }
+        });
 
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(graph);
@@ -46,16 +59,15 @@ public class Main extends Application {
     }
 
     class GraphPane extends StackPane {
-        Group root = new Group();
-        Canvas canvas = new Canvas();
+        Canvas canvas = new Canvas(400,270);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         public GraphPane() {
-            root.getChildren().add(canvas);
-            getChildren().add(root);
+            getChildren().add(canvas);
         }
 
         private void drawGraph() {
+            gc.clearRect(0,0,400,270);
             gc.setStroke(Color.BLACK);
             int max = 0;
             //find max
@@ -65,32 +77,39 @@ public class Main extends Application {
                 }
             }
             //draw graph
+            gc.strokeLine(0,250, 400, 250);
             for (int i = 0; i < 26; i++) {
                 double height = ((double)letters[i] / max) * 200;
                 double x = 10+(i*15);
                 gc.strokeRect(x, 250-height, 10, 250-(250-height));
+                gc.strokeText(String.valueOf(alpha[i]), x, 262);
             }
         }
     }
 
-    private void calculateOccurrence() throws IOException {
+    private void calculateOccurrence(String fileNameTxt) {
         //clear array
         for (int i = 0; i < 26; i++) {
             letters[i] = 0;
         }
         //make reader
-        BufferedReader in = new BufferedReader(new FileReader(new File("src/question4/hp.txt")));
-        //Read each character and count
-        int c;
-        while ((c = in.read()) != -1) {
-            char curr = (char)c;
-            for (int i = 0; i < 26; i++) {
-                if (Character.toLowerCase(curr) == alpha[i]) {
-                    letters[i] ++;
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(new File(fileNameTxt)));
+            //Read each character and count
+            int c;
+            while ((c = in.read()) != -1) {
+                char curr = (char) c;
+                for (int i = 0; i < 26; i++) {
+                    if (Character.toLowerCase(curr) == alpha[i]) {
+                        letters[i]++;
+                    }
                 }
             }
+        } catch (Exception e) {
+            System.out.println("FAILED TO READ FILE");
         }
     }
+
 
 
     public static void main(String[] args) {
